cd1a2d5739c33cce0efb723918ef08764a21b8ec
tlf <15022682570@sohu.com>
2018-05-23 16:35:45 +0800

    单个入口,开发和生产配置完成

--- src/b.js
+++ src/b.js
@@ -1,10 +1,10 @@
 import $ from "jquery";
 import { c } from "./c.js";
 export  function a(){
-    $("#container").text("ddd");
+    $("#container").text("adsa");
     $("#container").off("click");
     $("#container").on("click",e=>{
         console.log(c());
     })
 }
-console.log('bbbb');
+console.log('bbbb1');
--- src/c.js
+++ src/c.js
@@ -1,3 +1,3 @@
 export function c(){
-    return "vvvv232as1";
+    return "vvvv";
 }   
