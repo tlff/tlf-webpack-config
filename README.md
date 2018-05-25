d3f96993a791514becb3dc5443667c5ea75deeea
tlf <15022682570@sohu.com>
2018-05-25 17:49:53 +0800

    cli完成

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # tlf-webpack-config
-webpack配置脚手架
+webpack配置脚手架,自动创建配置好了webpack的项目.
+# 用法
+```
+twc create [name]
+```
--- bin/index-create.js
+++ bin/index-create.js
@@ -1,44 +1,45 @@
+#!/usr/bin/env node
 const program = require("commander");
 const fs = require('fs');
 const PATH = require("path");
 function run(name) {
-    copy("./public", name);
+    copy(PATH.resolve(__dirname, "../public"), name);
 }
 function copy(src, dst) {
     fs.exists(dst, function (exist) {
         if (!exist) {
-            fs.mkdir(dst);
-            fs.readdir(src, function (err, paths) {
-                if (err) {
-                    throw err;
-                }
-                paths.forEach(function (path) {
-                    var _src = PATH.resolve(src, path),
-                        _dst = PATH.resolve(dst, path);
-                    fs.stat(_src, function (err, stat) {
-                        if (err) {
-                            throw err;
-                        }
-                        if (stat.isFile()) {
-                            fs.readFile(_src, 'utf-8', function (err, data) {
-                                if (err) {
-                                    throw err;
-                                }
-
-                                fs.writeFile(_dst, data, 'utf-8', function (err) {
+            fs.mkdir(dst, function () {
+                fs.readdir(src, function (err, paths) {
+                    if (err) {
+                        throw err;
+                    }
+                    paths.forEach(function (path) {
+                        var _src = PATH.resolve(src, path),
+                            _dst = PATH.resolve(dst, path);
+                        fs.stat(_src, function (err, stat) {
+                            if (err) {
+                                throw err;
+                            }
+                            if (stat.isFile()) {
+                                fs.readFile(_src, 'utf-8', function (err, data) {
                                     if (err) {
                                         throw err;
                                     }
-                                    // console.log("%s 写入完毕", _dst);
-                                });
-                            })
-                        } else {
-                            copy(_src, _dst);
-                        }
-                    });
+
+                                    fs.writeFile(_dst, data, 'utf-8', function (err) {
+                                        if (err) {
+                                            throw err;
+                                        }
+                                        // console.log("%s 写入完毕", _dst);
+                                    });
+                                })
+                            } else {
+                                copy(_src, _dst);
+                            }
+                        });
+                    })
                 })
-            })
-            console.log('创建成功');
+            });
         } else {
             console.log("%s 已经存在", dst);
             return false;
